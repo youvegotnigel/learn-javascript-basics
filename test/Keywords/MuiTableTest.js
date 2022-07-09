8c8b77d969f22a36e79c7288e198c58b8bace4a8
Nigel Mulholland <nigeldave19@gmail.com>
2022-07-09 13:10:51 +0530

    Create MuiTableTest.js

---
+++ test/Keywords/MuiTableTest.js
@@ -0,0 +1,56 @@
+//const data = ['3', 'Jaime', 'Lannister', '45', 'Jaime Lannister']
+const data = ['D-4456', 'Soybeans', 'Elijah James', 'lodufbus@ruvetar.bm', '50,163', '85.611 %' ,'Filled']
+
+let updatedData = setupText(data)
+
+let myxpath = setUpXpath(updatedData)
+
+console.log(myxpath)
+
+function setUpXpath(cellInfo){
+
+    var xpath = ''
+    var count=1
+
+    cellInfo.forEach(element => {
+
+        var translateText = './/*[translate(normalize-space(.), \'ABCDEFGHIJKLMNOPQRSTUVWXYZ\', \'abcdefghijklmnopqrstuvwxyz\') = \''+element+'\']'
+
+        if(count == cellInfo.length){
+            xpath = xpath.concat(translateText)
+        }else{
+            xpath = xpath.concat(translateText, ' and ')
+        }
+        count = count + 1
+    });
+
+    let tag = '(//div[@role=\'grid\'])[2]/descendant-or-self::div[@role=\'row\']/descendant-or-self::div[' + xpath + ']'
+
+    return tag
+}
+
+
+
+
+
+
+
+function printArray(arguments){
+
+ 	for(var i=0; i<arguments.length; i++){
+ 		console.log(arguments[i]);
+    }
+}
+
+
+function setupText(arguments) {
+
+    let a = []
+
+    for(var i=0; i<arguments.length; i++){
+        //let stripedText = arguments[i].replace(/\s+/g, '')
+        let stripedText = arguments[i].trim()
+        a.push(stripedText.toLowerCase())
+    }
+    return a; 
+}
